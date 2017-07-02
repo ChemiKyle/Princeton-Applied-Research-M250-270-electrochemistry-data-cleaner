200434b3997d502493969772cabf54a2aa606cdb
ChemiKyle <Kyle.2493@gmail.com>
2017-07-02 10:07:28 -0400

    Added legend

--- plot_each_split.R
+++ plot_each_split.R
@@ -1,4 +1,5 @@
-# Plot each scan from a clean_split file in one color coded plot
+# Plot each scan from a clean_split file in one color coded plot for quick analysis
+# Not quite publication quality, but nice to visualize possible drift in successive scans
 
 fname = file.choose()
 
@@ -6,17 +7,18 @@ echem <- read.csv(fname)
 echem <- echem[colSums(!is.na(echem)) > 0] # get rid of the empty buffer columns
 title_name = tail(strsplit(fname, '/')[[1]], 1) # Get the file name without preceding path to use as title
 
-line_width = 2
-pairs = length(echem)/2 - 1
-color_list = c('red', 'orange', 'yellow', 'blue', 'purple', 'pink') # Create color vector to cycle through for scan number
-
 scan <- data.frame(echem[2], echem[1])
-plot(scan, type='l', lwd = line_width, xlab = 'V', ylab='A')
+line_width = 2
+plot(scan, type='l', lwd = line_width, xlab = 'V', ylab='A', col='red') # Initialize plot with first scan
+
+# Split off data from each scan and plot in a different color
+pairs = length(echem)/2
+color_list = rainbow(pairs) # Not sure if colorblind friendly :(
 
 for (i in 1:pairs) {
-  scan <- data.frame(echem[i*2], echem[1+i*2])
+  scan <- data.frame(echem[i*2], echem[i*2-1])
   lines(scan, lwd = line_width, col = color_list[i])
 }
 
 title(title_name, sub = "Individual scans")
-# I can't figure out how to get the legend function to work with this
+legend("topright", legend=c(1:pairs), col=color_list, lty=1, cex=0.8)
